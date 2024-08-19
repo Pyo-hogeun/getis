98a28c65dc9a6f63a75201bbf64f38bc1af6a446
Pyohogeun <pyohogeun@gmail.com>
2024-08-19 16:05:02 +0900

    feat : 마이페이지

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-function lnbToggle(){
-const $toggleOpen = document.querySelector('.toggle-open');
+function lnbToggle() {
+  const $toggleOpen = document.querySelector('.toggle-open');
   const $toggleClose = document.querySelector('.toggle-close');
   const $toggleLnb = document.querySelector('.toggle-lnb');
   const $contentWrap = document.querySelector('.content-wrap');
@@ -8,36 +8,34 @@ const $toggleOpen = document.querySelector('.toggle-open');
   const $gridContainers = document.querySelectorAll('.grid-container');
 
   // lnb 열기
-  $toggleOpen?.addEventListener('click', function(e){
+  $toggleOpen?.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('$contentWrap', $contentWrap.classList);
     $contentWrap.classList.remove('fold-lnb');
   })
   // lnb 접기
-  $toggleClose?.addEventListener('click', function(e){
+  $toggleClose?.addEventListener('click', function (e) {
     e.preventDefault();
     $contentWrap.classList.add('fold-lnb');
   })
   // lnb 토글
-  $toggleLnb?.addEventListener('click', function(e){
+  $toggleLnb?.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log($contentWrap.classList.contains('fold-lnb'));
 
-    if(!$contentWrap.classList.contains('fold-lnb')){
+    if (!$contentWrap.classList.contains('fold-lnb')) {
       $contentWrap.classList.add('fold-lnb');
     } else {
       $contentWrap.classList.remove('fold-lnb');
     }
   })
 }
-function tab(target){
+function tab(target) {
   const tabs = document.querySelectorAll(`${target} .tab`);
   const tabContents = document.querySelectorAll('.tab-content');
-  tabs.forEach(function(e){ 
+  tabs.forEach(function (e) {
     let targetTab = false;
-    e.addEventListener('click', function(e){
+    e.addEventListener('click', function (e) {
       e.preventDefault();
-      if(e.target.dataset.tab){
+      if (e.target.dataset.tab) {
         //target이 있을때만
         tabs.forEach(e => {
           e.classList.remove('on');
@@ -47,31 +45,30 @@ function tab(target){
         tabContents.forEach(e => {
           e.classList.remove('on');
         });
-        if(!targetTab.classList.contains('on')){
+        if (!targetTab.classList.contains('on')) {
           targetTab.classList.add('on')
         }
       }
     })
   })
 }
-function lnbAccordion(){
+function lnbAccordion() {
   const lnbAccordions = document.querySelectorAll('.has-depth');
-  lnbAccordions.forEach(function(acc){
-    acc.addEventListener('click', function(e){
+  lnbAccordions.forEach(function (acc) {
+    acc.addEventListener('click', function (e) {
       e.preventDefault();
       let trg = this.dataset.target;
       let depth = this.dataset.depth;
-      if(trg){
-        let targets = document.querySelectorAll('tr[data-menu="'+trg+'"]');
-        let targetsChild = document.querySelectorAll('[data-menu="'+trg+'"] ~ tr[data-]')
-        targets.forEach(function(e){
-          console.log('targets', e.style.display);
+      if (trg) {
+        let targets = document.querySelectorAll('tr[data-menu="' + trg + '"]');
+        let targetsChild = document.querySelectorAll('[data-menu="' + trg + '"] ~ tr[data-]')
+        targets.forEach(function (e) {
 
-          e.style.display !== 'none'?e.style.display='none'
-            :e.style.display = '';
+          e.style.display !== 'none' ? e.style.display = 'none'
+            : e.style.display = '';
         })
       }
-      if(!this.classList.contains('open')){
+      if (!this.classList.contains('open')) {
         this.classList.add('open');
       } else {
         this.classList.remove('open');
@@ -79,90 +76,93 @@ function lnbAccordion(){
     })
   })
 }
-function windowResizer(target, handler, direction){
+function windowResizer(target, handler, direction) {
   const resizeHandler = document.querySelector(handler);
   const resizeTarget = document.querySelector(target);
   let defaultPositionInfo = {
-    x : 0,
-    y : 0,
-    aTargetHeight : 0,
-    aTargetWidth : 0,
-    bTargetHeight : 0,
-    bTargetWidth : 0
+    x: 0,
+    y: 0,
+    aTargetHeight: 0,
+    aTargetWidth: 0,
+    bTargetHeight: 0,
+    bTargetWidth: 0
   }
   const handlerMouseDown = (e) => {
     defaultPositionInfo.x = e.clientX;
     defaultPositionInfo.y = e.clientY;
-    console.log(defaultPositionInfo.x , defaultPositionInfo.y, resizeTarget.offsetHeight);
   }
 
   document.querySelector(handler).addEventListener('mousedown', handlerMouseDown);
 }
-
 // 화면 가로사이즈 조절
-function gridColResize(){
+function gridColResize() {
   const handler = document.querySelector('button.handler');
   const gridContainers = document.querySelectorAll('.grid-container');
-  
+
   let posX = 0;
   let gridWidthFirst = 0;
   let gridWidthSecond = 0;
 
   handler.addEventListener('mousedown', handlerMouseDown);
-  function handlerMouseDown(e){
+  function handlerMouseDown(e) {
     posX = e.clientX;
     gridWidthFirst = gridContainers[0].clientWidth;
     gridWidthSecond = gridContainers[1].clientWidth;
     document.addEventListener('mousemove', handlerMouseMove);
     document.addEventListener('mouseup', handlerMouseUp);
   }
-  function handlerMouseMove(e){
+  function handlerMouseMove(e) {
     let move = e.clientX - posX;
 
-    console.log(gridWidthFirst);
     gridContainers[0].style.flexBasis = gridWidthFirst + move + 'px';
     gridContainers[1].style.flexBasis = gridWidthSecond - move + 'px';
   }
-  function handlerMouseUp(){
+  function handlerMouseUp() {
     document.removeEventListener('mousemove', handlerMouseMove);
   }
 }
-function drop(e){
-  console.log('drop!!');
+function editFavorite(e) {
 }
-
-function editFavorite(e){
-  console.log('edit!', e);
-}
-
-function combo(){
+// 콤보박스
+function combo() {
   let combos = document.querySelectorAll('.combo');
-  combos?.forEach(function(combo){
-    combo.addEventListener('click',function(e){
-      if(this.classList.contains('focus')){
+  combos?.forEach(function (combo) {
+    combo.addEventListener('click', function (e) {
+      if (this.classList.contains('focus')) {
         this.classList.remove('focus');
       } else {
         this.classList.add('focus');
       }
-      // console.log(options);
     });
     let options = document.querySelectorAll('.options > li');
-    options?.forEach(function(e){
-      e.addEventListener('click', function(option){
-        console.log('option click');
+    options?.forEach(function (e) {
+      e.addEventListener('click', function (option) {
         let comboWrap = option.target.closest('.combo');
         let selectedValue = option.target.innerHTML;
-        console.log(comboWrap.querySelector('.label'));
         comboWrap.querySelector('.label').innerHTML = selectedValue;
       });
     })
   });
 }
-document.addEventListener('DOMContentLoaded', function(){
+// gnb
+function gnbSubmenu(){
+  const gnbMenu = document.querySelectorAll('.gts-gnb .menu-list > ul > li');
+  gnbMenu.forEach(function(e){
+    e.addEventListener('mouseenter', function(menu){
+      console.log(menu.target.querySelector('.sub-menu'));
+      if (menu.target.querySelector('.sub-menu')) menu.target.querySelector('.sub-menu').style.display = 'block';
+    });
+    e.addEventListener('mouseleave', function(menu){
+      if (menu.target.querySelector('.sub-menu')) menu.target.querySelector('.sub-menu').style.display = 'none';
+    });
+  });
+}
+document.addEventListener('DOMContentLoaded', function () {
   lnbToggle();
   tab('.gts-lnb');
   lnbAccordion();
   combo();
+  gnbSubmenu();
 });
 
 
