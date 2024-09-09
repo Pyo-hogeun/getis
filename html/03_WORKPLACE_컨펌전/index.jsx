7cb1cefa492da878b70a924b4bd98ecbfd8e6de3
Pyohogeun <pyohogeun@gmail.com>
2024-09-09 23:50:58 +0900

    feat : 캘린더(작업중)

--- html/03_WORKPLACE_컨펌전/index.jsx
+++ html/03_WORKPLACE_컨펌전/index.jsx
@@ -1,5 +1,96 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/ko';
+
+const Toolbar = (props) => {
+  const {
+    date,
+  } = props;
+  const navigate = (action) => {
+    props.onNavigate(action);
+  };
+  return (
+    <div className="gts-calendar-toolbar">
+      <div className="prefix">
+        <button
+          type="button"
+          onClick={navigate.bind(null, 'PREV')}
+          class="gts-calendar-prev-month"
+        >
+          &lt;
+        </button>
+        <button
+          type="button"
+          onClick={navigate.bind(null, 'NEXT')}
+          class="gts-calendar-next-month"
+        >
+          &gt;
+        </button>
+        <button type="button" onClick={navigate.bind(null, 'TODAY')}>
+          오늘
+        </button>
+      </div>
+      <div className="center">
+        <span className="rbc-btn-group">
+          <img src="../../assets/images/icon_calendar.png" className="icon-calendar"/>
+          <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
+        </span>
+      </div>
+      <div className="suffix">
+        <a href="" class="btn tertiary">
+          <i class="icon-excel-download prefix"></i>
+          엑셀다운로드
+        </a>
+        <a href="" class="btn tertiary">
+          <i class="icon-excel-upload prefix"></i>
+          엑셀업로드
+        </a>
+        <a href="" class="btn tertiary">
+          <i class="icon-printer prefix"></i>
+          프린터
+        </a>
+      </div>
+    </div>
+  );
+}
+const message = {
+  week: '주',
+  work_week: '평일',
+  day: '일',
+  month: '월',
+  previous: '전',
+  next: '후',
+  today: '오늘',
+  agenda: '아젠다',
+
+  showMore: (total) => `+${total} 더보기`,
+}
+const DayHeader = (props) => {
+  return(props.date);
+}
+const MyCalendar = (props) => {
+  moment.locale('ko');
+  const localizer = momentLocalizer(moment);
+  return(
+    <div className="myCustomHeight">
+      <Calendar
+        localizer={localizer}
+        // events={myEventsList}
+        message={message}
+        startAccessor="start"
+        endAccessor="end"
+        components={{
+          toolbar: Toolbar,
+          day: {
+            header: DayHeader,
+          }
+        }}
+      />
+    </div>
+  );
+}
 
 // Clear the existing HTML content
 const calendar = document.querySelector('#calendar');
@@ -7,4 +98,4 @@ const calendar = document.querySelector('#calendar');
 // Render your React component instead
 const root = createRoot(calendar);
  
-root.render(<h1>Hello, world</h1>);
+root.render(<MyCalendar/>);
