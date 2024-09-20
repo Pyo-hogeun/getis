70d1fbe313cb65b53d2a57846afaa8c88ba7447a
Pyohogeun <pyohogeun@gmail.com>
2024-09-20 14:02:03 +0900

    feat : 멀티선택 select 추가

---
+++ html/00_COMPONENT/MultieSelect.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Select, Space } from 'antd';
+import { css } from '@emotion/css';
+import iconArrowSelect from '../../assets/images/icon_select_arrow.png';
+const IconArrowSelect = () => {
+  return <img src={iconArrowSelect} />
+}
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const options = [
+  {
+    label: 'China',
+    value: 'china',
+    emoji: '🇨🇳',
+    desc: 'China (中国)',
+  },
+  {
+    label: 'USA',
+    value: 'usa',
+    emoji: '🇺🇸',
+    desc: 'USA (美国)',
+  },
+  {
+    label: 'Japan',
+    value: 'japan',
+    emoji: '🇯🇵',
+    desc: 'Japan (日本)',
+  },
+  {
+    label: 'Korea',
+    value: 'korea',
+    emoji: '🇰🇷',
+    desc: 'Korea (韩国)',
+  },
+];
+const MultieSelect = () => {
+  return (<>
+    <Select
+      mode="multiple"
+      style={{
+        width: '300px',
+      }}
+      className={multieSelectStyle}
+      placeholder="select one country"
+      defaultValue={['china']}
+      onChange={handleChange}
+      options={options}
+      optionRender={(option) => (
+        <Space>
+          <span role="img" aria-label={option.data.label}>
+            {option.data.emoji}
+          </span>
+          {option.data.desc}
+        </Space>
+      )}
+      suffixIcon={<IconArrowSelect />}
+    />
+  </>)
+}
+const multieSelectStyle = css`
+  && .ant-select-selector{
+    background-color: #ffffff;
+    border: 1px solid #dddddd;
+    border-radius: 3px;
+    height: 32px;
+    padding-left: 10px;
+    color: #333333;
+    font-size: 14px;
+  }
+`
+// Clear the existing HTML content
+const $multieSelect = document.querySelector('#gtsMultieSelect');
+
+// Render your React component instead
+const root = createRoot($multieSelect);
+
+root.render(<MultieSelect />);
